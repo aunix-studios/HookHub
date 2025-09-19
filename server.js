cb36673d804ce8d2cf9356b4d820107d24bf7f1d
Aunix Studios <businessmine113@gmail.com>
2025-09-19 11:42:07 +0600

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Serve frontend
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
 // OAuth2 callback
